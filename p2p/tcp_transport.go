50ce3f38909e0c7834c339ef379ce3dae4fe00da
amanycodes <amanycodes@gmail.com>
2025-07-06 02:58:15 +0530

    add basic encoder and message struct
    Signed-off-by: amanycodes <amanycodes@gmail.com>

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -74,19 +74,14 @@ func (t *TCPTransport) handleConn(conn net.Conn) {
 		return
 	}
 
-	buf := make([]byte, 2000)
-	// msg := &Temp{}
+	msg := &RPC{}
 	for {
-		n, err := conn.Read(buf)
-		if err != nil {
+		if err := t.Decoder.Decode(conn, msg); err != nil {
 			fmt.Printf("TCP error: %s\n", err)
 			continue
 		}
-		// if err := t.Decoder.Decode(conn, msg); err != nil {
-		// 	fmt.Printf("TCP error: %s\n", err)
-		// 	continue
-		// }
-		fmt.Printf("message: %+v\n", buf[:n])
+		msg.From = conn.RemoteAddr()
+		fmt.Printf("message: %+v\n", msg)
 	}
 
 }
--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -2,7 +2,6 @@ package p2p
 
 import (
 	"testing"
-	"time"
 
 	"gotest.tools/v3/assert"
 )
@@ -12,18 +11,10 @@ func TestNewTCPTransport(t *testing.T) {
 	opts := TCPTransportOpts{
 		ListenAddr:    listenAddr,
 		HandshakeFunc: NOPHandshakeFunc,
-		Decoder:       GOBDecoder{},
+		Decoder:       DefaultDecoder{},
 	}
 	tr := NewTCPTransport(opts)
 
 	assert.Equal(t, tr.ListenAddr, listenAddr)
 
-	err := tr.ListenAndAccept()
-	assert.NilError(t, err)
-
-	// Give the listener a moment to start, then close it to end the test
-	time.Sleep(100 * time.Millisecond)
-	if tr.listener != nil {
-		tr.listener.Close()
-	}
 }
